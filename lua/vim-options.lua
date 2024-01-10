191bbf669b7aca08af9fbf9c7a9617132a7e070f
Danny Krämer <danny-kraemer@outlook.com>
2024-01-10 19:33:14 +0100

    initial commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.wo.number = true
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
