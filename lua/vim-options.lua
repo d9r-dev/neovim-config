b0f0fae714992a342ea797dc813c30706c79f276
Danny Krämer <d.kraemer@dotsource.de>
2024-01-16 10:06:11 +0100

    add some basic options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,3 +7,10 @@ vim.g.mapleader = " "
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.wo.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'
+
