7a6946ce0ed84ee47929660c69091a513a98bf9d
Danny Krämer <d.kraemer@dotsource.de>
2024-02-29 09:21:22 +0100

    new config

---
+++ lua/custom/init.lua
@@ -0,0 +1,13 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
